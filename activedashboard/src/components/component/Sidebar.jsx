8cf5dff4d8a510538d20d5db3257fa7547329ad7
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-03-28 12:24:50 -0300

    Inicializado proyecto, añadido sidebars y cards

---
+++ activedashboard/src/components/component/Sidebar.jsx
@@ -0,0 +1,147 @@
+"use client";
+import { useState } from "react";
+
+function Sidebar() {
+  const [sidenav, setSidenav] = useState(false);
+
+  const toggleSidenav = () => {
+    setSidenav(!sidenav);
+  };
+
+  return (
+    <div
+      id="sidebar"
+      className="fixed left-0 top-0 bottom-0 h-full flex flex-row"
+      data-sidenav={sidenav}
+    >
+      <button
+        onClick={toggleSidenav}
+        className="p-2 border-2 bg-white rounded-md border-gray-200 shadow-lg text-gray-500 focus:bg-teal-500 focus:outline-none focus:text-white absolute top-0 left-0 sm:hidden"
+      >
+        <svg
+          className="w-5 h-5 fill-current"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </button>
+      <div
+        className={`bg-white h-screen md:block shadow-xl px-3 w-30 md:w-60 lg:w-60 overflow-x-hidden transition-transform duration-300 ease-in-out ${
+          sidenav ? "block" : "hidden"
+        }`}
+      >
+        <div className="space-y-6 md:space-y-10 mt-10">
+          <h1 className="font-bold text-4xl text-center md:hidden">
+            D<span className="text-teal-600">.</span>
+          </h1>
+          <h1 className="hidden md:block font-bold text-sm md:text-xl text-center">
+            Sistema de Bienes<span className="text-teal-600">.</span>
+          </h1>
+          <div id="profile" className="space-y-3">
+            <img
+              src="https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              alt="Avatar user"
+              className="w-10 md:w-16 rounded-full mx-auto"
+            />
+            <div>
+              <h2 className="font-medium text-xs md:text-sm text-center text-teal-500">
+                Nombre
+              </h2>
+              <p className="text-xs text-gray-500 text-center">Administrator</p>
+            </div>
+          </div>
+          <div className="flex border-2 border-gray-200 rounded-md focus-within:ring-2 ring-teal-500">
+            <input
+              type="text"
+              className="w-full rounded-tl-md rounded-bl-md px-2 py-3 text-sm text-gray-600 focus:outline-none"
+              placeholder="Search"
+            />
+            <button className="rounded-tr-md rounded-br-md px-2 py-3 hidden md:block">
+              <svg
+                className="w-4 h-4 fill-current"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div id="menu" className="flex flex-col space-y-2">
+            <a
+              href=""
+              className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
+            >
+              <svg
+                width="30px"
+                height="30px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4.59375 10.2656L11.0769 12.6857L3 16.5217V7.94203L12 4L21 7.71014V16.5217L12.675 20V11.4203L19 8.85239"
+                  stroke="#000000"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <span className="">Herramientas</span>
+            </a>
+            {/* Agrega más enlaces aquí */}
+            <a
+              href=""
+              className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
+            >
+              <svg
+                width="30px"
+                height="30px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M13 20V18C13 15.2386 10.7614 13 8 13C5.23858 13 3 15.2386 3 18V20H13ZM13 20H21V19C21 16.0545 18.7614 14 16 14C14.5867 14 13.3103 14.6255 12.4009 15.6311M11 7C11 8.65685 9.65685 10 8 10C6.34315 10 5 8.65685 5 7C5 5.34315 6.34315 4 8 4C9.65685 4 11 5.34315 11 7ZM18 9C18 10.1046 17.1046 11 16 11C14.8954 11 14 10.1046 14 9C14 7.89543 14.8954 7 16 7C17.1046 7 18 7.89543 18 9Z"
+                  stroke="#000000"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              <span className="">Garantias</span>
+            </a>
+            <a
+              href=""
+              className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
+            >
+              <svg
+                width="30px"
+                height="30px"
+                viewBox="0 0 1024 1024"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="#000000"
+                  d="m665.216 768 110.848 192h-73.856L591.36 768H433.024L322.176 960H248.32l110.848-192H160a32 32 0 0 1-32-32V192H64a32 32 0 0 1 0-64h896a32 32 0 1 1 0 64h-64v544a32 32 0 0 1-32 32H665.216zM832 192H192v512h640V192zM352 448a32 32 0 0 1 32 32v64a32 32 0 0 1-64 0v-64a32 32 0 0 1 32-32zm160-64a32 32 0 0 1 32 32v128a32 32 0 0 1-64 0V416a32 32 0 0 1 32-32zm160-64a32 32 0 0 1 32 32v192a32 32 0 1 1-64 0V352a32 32 0 0 1 32-32z"
+                />
+              </svg>
+              <span className="">Respuestos</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
